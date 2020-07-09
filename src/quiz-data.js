f931439477e7f9c0115172e3a0a878418055a034
Irena <irenejpopova6@gmail.com>
2020-07-09 10:39:09 +0200

    creating quizz-data

---
+++ src/quiz-data.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const QUESTION_DATA = [
+  {
+    question: <span>Which of the following is TRUE about reflows and repaints?</span>,
+    answers: [
+      <span>They are the same thing.</span>,
+      <span>Repaints (or redraws) occur when elements change their visual styles but not layout.</span>,
+      <span>Reflows compute layout, are more performance critical, and occur when elements are inserted, removed, moved, animated, etc.</span>,
+      <span>The previous two answers are correct.</span>
+    ],
+    correct: 3
+  },
+
+
